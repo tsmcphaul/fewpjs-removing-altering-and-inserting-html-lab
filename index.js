bc016640581083c4bdd30ef3c2d651ea6aa76b75
Tammarra McPhaul <shaemc22@gmail.com>
2021-11-10 20:15:41 -0500

    commit tree d8fad03d235a610310d2dc7eb407b2f640a6fb82

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.querySelector('main#main')
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
